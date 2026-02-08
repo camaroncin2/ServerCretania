7cb41bb73cc02d82fd13093c1a504d635832e8ad
camaroncin2 <jdavid1201.md@gmail.com>
2026-02-08 09:43:24 -0600

    Add files via upload

--- startup_scripts/mehanical_press_items.js
+++ startup_scripts/mehanical_press_items.js
@@ -1,4 +1,7 @@
 StartupEvents.registry('item', event => {
   event.create('steel_coated_rod').displayName('Varilla de Acero Revestido')
   event.create('coated_casing').displayName('Carcasa Revestida').maxStackSize(64).parentModel('kubejs:item/coated_casing')
+  event.create('incomplete_press_casing_mk1').displayName('Carcasa de Prensa Incompleta').maxStackSize(64).parentModel('kubejs:item/incomplete_press_casing_mk1')
+  event.create('incomplete_press_casing_mk2').displayName('Carcasa de Prensa Incompleta').maxStackSize(64).parentModel('kubejs:item/incomplete_press_casing_mk2')
+  event.create('incomplete_press_casing_mk3').displayName('Carcasa de Prensa Incompleta').maxStackSize(64).parentModel('kubejs:item/incomplete_press_casing_mk3')
 })
