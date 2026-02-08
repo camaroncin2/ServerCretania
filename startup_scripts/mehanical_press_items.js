f0f8a0dfffe4e8f3aea2ab8ac6890ff5343ae52c
camaroncin2 <jdavid1201.md@gmail.com>
2026-02-08 13:22:37 -0600

    Enhance mechanical press items with hardness and tool requirements
    Added hardness and tool requirement to mechanical press piston and piston extension.

--- startup_scripts/mehanical_press_items.js
+++ startup_scripts/mehanical_press_items.js
@@ -1,12 +1,12 @@
 StartupEvents.registry('item', event => {
   event.create('steel_coated_rod').displayName('Varilla de Acero Revestido')
   event.create('coated_casing').displayName('Carcasa Revestida').maxStackSize(64).parentModel('kubejs:item/coated_casing')
-  event.create('mechanical_press_piston').displayName('Piston de Prensa Mecánica').maxStackSize(64).parentModel('kubejs:item/mechanical_press_piston')
 })
 
 StartupEvents.registry('block', event => {
   event.create('incomplete_press_casing_mk1').displayName('Carcasa de Prensa Incompleta').hardness(3.0).requiresTool(true)
   event.create('incomplete_press_casing_mk2').displayName('Carcasa de Prensa Incompleta').hardness(3.0).requiresTool(true)
   event.create('incomplete_press_casing_mk3').displayName('Carcasa de Prensa Incompleta').hardness(3.0).requiresTool(true)
-  event.create('piston_extension_half').displayName('Mitad de Extensor')
+  event.create('piston_extension_half').displayName('Mitad de Extensor').hardness(3.0).requiresTool(true)
+  event.create('mechanical_press_piston').displayName('Piston de Prensa Mecánica').hardness(3.0).requiresTool(true)
 })
